1855622086633588fea7e99f459b996037381e2f
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-06-19 00:08:09 -0500

    Tuesdays Effect

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,56 +1,15 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import './App.css';
+import FriendContainer from "./components/friendContainer";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state= {
-      friends: [],
-      name: "",
-      age: "",
-      email: ""
-    };
+const App = () => {
+    return (
+      <div>
+      <FriendContainer />
+      </div>
+    );
   }
 
-  componentDidMount() {
-    console.log("inside CDM");
-    // fetch('http://localhost:5000/friends')
-    //   .then(res => res.json())
-    //   .then(friends => console.log("fetch completed");)
-    //   .catch(err => console.log(err));
-    // console.log("below fetch");
-  }
 
-  render () {
-  return (
-    <div>
-      <form className="friend-form">
-        <input 
-        className="friend-form-name"
-        placeholder="name"
-        value={this.state.name}
-        name="age"
-        />
-
-        <input 
-        className="friend-form-age"
-        placeholder="age"
-        value={this.state.age}
-        name="age"
-        />
-        
-        <input 
-        className="friend-form-email"
-        placeholder="email"
-        value={this.state.email}
-        name="age"
-        />
-        <button className="btn">Add Friend</button>
-      </form>       
-    </div>
-  );
-}
-}
 export default App;
